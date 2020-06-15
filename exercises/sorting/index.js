4367ac90790bd011ee14689ba5e844b6cfeb51b9
Arthur <ma2bernier@gmail.com>
2020-06-15 12:09:10 -0400

    updates

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,6 +1,9 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+// [0]
+
+
 const  bubbleSort = (arr) => {
 let swaps = Infinity
 while(swaps > 0){
@@ -32,8 +35,6 @@ function insertionSort(arr){
   return arr
 }
 
-console.log(insertionSort([10,30,99,1,456,22]))
-
 
 
 function selectionSort(arr) {
@@ -41,11 +42,61 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length <= 1){
+    return arr
+  }
+
+  const centerPoint = Math.floor(arr.length / 2) // 7/2 = 3.5
+  const left = arr.slice(0,centerPoint);
+  const right = arr.slice(centerPoint);
+
+  return merge(mergeSort(left), mergeSort(right))
 
 }
 
 function merge(left, right) {
+    const results = [];
 
+    while(left.length && right.length){
+      if(left[0] < right[0]){
+        results.push(left.shift())
+      } else {
+        results.push(right.shift())
+      }
+    }
+
+    return [...results, ...left, ...right]
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort, merge, insertionSort };
+
+// [3, 7, 9, 2, 14, 5, 13, 22]
+
+[3,7,9,2,5,13],14,[22]
+[3,7,9,2,5,13]
+ 2,3,7,9,5,13
+ [3,7,9]
+// 4[]            //4[]
+// 2[] 2[]        // 2[] 2[]
+// 1[] 1[] 1 [] 1[]  // 1[] 1[] 1[] 1[]
+
+
+function quickSort(arr){
+ if(arr.length < 2){
+   return arr
+ }
+ const midpointIndex = Math.floor(arr.length/2);
+ const pivot = arr[midpointIndex]
+ const myLeftArr = [];
+ const myRightArr = [];
+
+ for(let i = 0; i < arr.length; i++){
+  if(i !== midpointIndex){
+    arr[i] < pivot ?
+      myLeftArr.push(arr[i]):
+      myRightArr.push(arr[i]);
+  }
+ }
+ return [...quickSort(myLeftArr), pivot, ...quickSort(myRightArr)]
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge, insertionSort, quickSort };
